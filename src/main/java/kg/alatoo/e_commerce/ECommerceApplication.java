e9476130e7cd463d28bcf04a10ee897e96741cb9
AkbekovNursultan <nursultan20052003@gmail.com>
2025-03-14 20:26:48 +0600

    Initial commit

---
+++ src/main/java/kg/alatoo/e_commerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
